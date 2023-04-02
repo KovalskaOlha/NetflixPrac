91fbbff39dc8a072e6ba68202a45f173af7940f2
KovalskaOlha <unlimolg@gmail.com>
2023-04-02 15:49:31 -0400

    Main and Films classes are created

---
+++ src/Films.java
@@ -0,0 +1,5 @@
+public class Films {
+    String name;
+    String genre;
+    String year;
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Films f=new Films();
+        f.genre="comedy";
+        f.name="Handover";
+        f.year="2010";
+    }
+}
